b5791a0ed0c770b283940418d108297d5173838f
kirinokirino <kirinokirino@lunariko.com>
2023-01-19 14:16:34 +0300

    Separate choose_input_port from midir example.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::io::{stdin, stdout, Write};
 
-use midir::{Ignore, MidiInput};
+use midir::{Ignore, MidiInput, MidiInputPort};
 
 fn main() {
     match run() {
@@ -14,40 +14,13 @@ fn run() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
 
     let mut midi_in = MidiInput::new("midir reading input")?;
-    midi_in.ignore(Ignore::None);
-
-    // Get an input port (read from console if multiple are available)
-    let in_ports = midi_in.ports();
-    let in_port = match in_ports.len() {
-        0 => return Err("no input port found".into()),
-        1 => {
-            println!(
-                "Choosing the only available input port: {}",
-                midi_in.port_name(&in_ports[0]).unwrap()
-            );
-            &in_ports[0]
-        }
-        _ => {
-            println!("\nAvailable input ports:");
-            for (i, p) in in_ports.iter().enumerate() {
-                println!("{}: {}", i, midi_in.port_name(p).unwrap());
-            }
-            print!("Please select input port: ");
-            stdout().flush()?;
-            let mut input = String::new();
-            stdin().read_line(&mut input)?;
-            in_ports
-                .get(input.trim().parse::<usize>()?)
-                .ok_or("invalid input port selected")?
-        }
-    };
-
+    let in_port = choose_input_port(&mut midi_in, "UMX 61")?;
     println!("\nOpening connection");
-    let in_port_name = midi_in.port_name(in_port)?;
+    let in_port_name = midi_in.port_name(&in_port)?;
 
     // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
     let _conn_in = midi_in.connect(
-        in_port,
+        &in_port,
         "midir-read-input",
         move |stamp, message, _| {
             println!("{}: {:?} (len = {})", stamp, message, message.len());
@@ -65,4 +38,43 @@ fn run() -> Result<(), Box<dyn Error>> {
 
     println!("Closing connection");
     Ok(())
+}
+
+pub fn choose_input_port(
+    midi_in: &mut MidiInput,
+    expected_port_name: &str,
+) -> Result<MidiInputPort, Box<dyn Error>> {
+    midi_in.ignore(Ignore::None);
+
+    // Get an input port (read from console if multiple are available)
+    let in_ports = midi_in.ports();
+    let in_port = match in_ports.len() {
+        0 => return Err("no input port found".into()),
+        1 => {
+            println!(
+                "Choosing the only available input port: {}",
+                midi_in.port_name(&in_ports[0]).unwrap()
+            );
+            &in_ports[0]
+        }
+        _ => {
+            println!("\nAvailable input ports:");
+            for (i, p) in in_ports.iter().enumerate() {
+                let name = midi_in.port_name(p).unwrap();
+                println!("{}: {}", i, name);
+                if name.contains(expected_port_name) {
+                    println!("Found port {name}");
+                    return Ok(p.clone());
+                }
+            }
+            print!("Please select input port: ");
+            stdout().flush()?;
+            let mut input = String::new();
+            stdin().read_line(&mut input)?;
+            in_ports
+                .get(input.trim().parse::<usize>()?)
+                .ok_or("invalid input port selected")?
+        }
+    };
+    Ok(in_port.clone())
 }
