821a629e43cf04858adc17c4d0d03d3c1dd0f580
kirinokirino <kirinokirino@lunariko.com>
2023-01-19 13:24:32 +0300

    Structure a basic sampler loop.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,61 @@
-use quad_snd::{AudioContext, Sound, PlaySoundParams};
+use quad_snd::{AudioContext, PlaySoundParams, Sound};
+
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
-    let mut ctx = AudioContext::new();
-    let mut sound = Sound::load(&mut ctx, include_bytes!("t.wav"));
-	let params = PlaySoundParams { looped: true, volume: 0.1 };
-    sound.play(&mut ctx, params);
+    let mut sampler = Sampler::new();
+    sampler.add_one_shot(include_bytes!("t.wav"));
+    sampler.run();
+}
 
-    loop {}
+struct Sample {
+    sound: Sound,
+    params: PlaySoundParams,
+}
+
+impl Sample {
+    pub fn new(sound: Sound, params: PlaySoundParams) -> Self {
+        Self { sound, params }
+    }
+
+    pub fn play(&self, ctx: &mut AudioContext) {
+        let PlaySoundParams { looped, volume } = self.params;
+        self.sound.play(ctx, PlaySoundParams { looped, volume });
+    }
+}
+
+struct Sampler {
+    ctx: AudioContext,
+    one_shots: Vec<Sample>,
+    loops: Vec<Sample>,
+}
+
+impl Sampler {
+    pub fn new() -> Self {
+        let mut ctx = AudioContext::new();
+
+        Self {
+            ctx,
+            one_shots: Vec::new(),
+            loops: Vec::new(),
+        }
+    }
+
+    pub fn add_one_shot(&mut self, data: &[u8]) {
+        let sound = Sound::load(&self.ctx, include_bytes!("t.wav"));
+        let params = PlaySoundParams {
+            looped: false,
+            volume: 0.1,
+        };
+        let sample = Sample::new(sound, params);
+        self.one_shots.push(sample);
+    }
+
+    pub fn run(&mut self) -> ! {
+        loop {
+            self.one_shots.get(0).unwrap().play(&mut self.ctx);
+            sleep(Duration::from_secs(10));
+        }
+    }
 }
